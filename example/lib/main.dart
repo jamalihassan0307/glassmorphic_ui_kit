c19cb20fb89043c01482ed5fdb5eff797695d658
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-02 22:24:38 +0500

    Add route for TabBarScreen in main.dart
    - Included the TabBarScreen route in the main application, enabling navigation to the new tab interface.
    - Updated import statements to incorporate TabBarScreen, enhancing the overall navigation structure.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/navigation/navigation_bar_screen.dart';
 import 'screens/navigation/navigation_drawer_screen.dart';
 import 'screens/navigation/navigation_rail_screen.dart';
+import 'screens/navigation/tab_bar_screen.dart';
 import 'widgets/navigation/app_drawer.dart';
 import 'widgets/navigation/app_navigation_bar.dart';
 
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         '/navigation/bar': (context) => const NavigationBarScreen(),
         '/navigation/drawer': (context) => const NavigationDrawerScreen(),
         '/navigation/rail': (context) => const NavigationRailScreen(),
+        '/navigation/tabs': (context) => const TabBarScreen(),
         // Add more routes here as we create more screens
       },
     );
