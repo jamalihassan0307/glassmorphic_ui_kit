db34f3765aba5b7c87016463a8493391f1690edf
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-02 19:52:43 +0500

    Refactor font assets: create Assets class for font management and remove individual Roboto font files.

---
+++ lib/generated/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+  Assets._();
+  
+  // Fonts
+  static const String fontRobotoRegular = 'assets/fonts/Roboto-Regular.ttf';
+  static const String fontRobotoBold = 'assets/fonts/Roboto-Bold.ttf';
+}
