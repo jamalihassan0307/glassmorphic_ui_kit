8290f1cb374ffd34d6e5dc413d19eeefd48c1023
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-02 19:54:23 +0500

    Add unit test for Roboto font assets to ensure they are correctly bundled in the project.

---
+++ test/font_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Roboto fonts are bundled', () async {
+    final regularFont = rootBundle.load('packages/glassmorphic_ui_kit/assets/fonts/Roboto-Regular.ttf');
+    final boldFont = rootBundle.load('packages/glassmorphic_ui_kit/assets/fonts/Roboto-Bold.ttf');
+
+    expect(regularFont, completes);
+    expect(boldFont, completes);
+  });
+} 
