636c57ed8dccab0867b8b74566f837d17046b30d
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-02 17:59:48 +0500

    Add GlassConstants class to define constants for blur and opacity settings in glassmorphic UI

---
+++ lib/src/utils/glass_constants.dart
@@ -0,0 +1,8 @@
+class GlassConstants {
+  static const double defaultBlur = 10.0;
+  static const double defaultOpacity = 0.2;
+  static const double minBlur = 0.0;
+  static const double maxBlur = 30.0;
+  static const double minOpacity = 0.0;
+  static const double maxOpacity = 1.0;
+} 
